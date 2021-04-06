70a7c324e5b084298db772828833b5fa37b0df16
vanilton filho <vanilton.alv3sfilho@gmail.com>
2021-04-05 23:59:04 -0300

    add atividade 3

--- src/main/java/peerudp/PeerChat.java
+++ src/main/java/peerudp/PeerChat.java
@@ -6,15 +6,16 @@ import java.net.InetAddress;
 import peerudp.peer.Peer;
 import peerudp.server.RecordServer;
 
-/**
- * Hello world!
- *
- */
+
 public class PeerChat {
     public static void main(String[] args) throws NumberFormatException, IOException {
-        if (args.length == 2 && args[0].equals("--run-server")) {
-            new RecordServer(Integer.parseInt(args[1]));
-        } else if (args.length == 5 && args[0].equals("--run-peer") && args[1].equals("--to-register")) {
+        if (args.length == 3 && args[0].equals("--run-server")) {
+            new RecordServer(Integer.parseInt(args[1]), InetAddress.getLocalHost(), Integer.parseInt(args[2]), false, false);
+        } else if (args.length == 5 && args[0].equals("--run-server") && args[1].equals("--to-replicate")) {
+
+            new RecordServer(Integer.parseInt(args[2]), InetAddress.getByName(args[3]), Integer.parseInt(args[4]), false, true);
+        }
+        else if (args.length == 5 && args[0].equals("--run-peer") && args[1].equals("--to-register")) {
             var peer = new Peer(Integer.parseInt(args[2]), InetAddress.getByName(args[3]), Integer.parseInt(args[4]),
                     true);
             peer.execute();
---
+++ src/main/java/peerudp/server/ServerRecord.java
@@ -0,0 +1,2 @@
+package peerudp.server;public class ServerRecord {
+}
